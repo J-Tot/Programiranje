d88837db3976c66eb6d09369fc854d03a773086d
JTot <mario.jakov.tot@gmail.com>
2026-09-23 19:44:31 +0200

    Auti finalna verzija
    verzija 2

--- auti/main.cpp
+++ auti/main.cpp
@@ -5,26 +5,58 @@ using namespace std;
 int main()
 {
 
-string brand[5]={
-"Ferrari",
-"Mercedes",
-"Renault",
-"Porsche",
-"BMW"
-};
-string model[5]={
-"SF24",
-"G Klasa",
-"Clio",
-"911",
-"X5",
-};
+    string brand[5]=
+    {
+        "Ferrari",
+        "Mercedes",
+        "Renault",
+        "Porsche",
+        "BMW"
+    };
+    string model[20]=
+    {
+
+        "SF24",
+        "LaFerrari",
+        "Spider",
+        "Portofino",
+
+        "G Klasa",
+        "A Klasa",
+        "B Klasa",
+        "C Klasa",
+
+        "Clio",
+        "Scenic",
+        "Megane",
+        "Espace",
+
+        "911",
+        "718",
+        "Taycan",
+        "Panamera",
+
+        "X5",
+        "X1",
+        "X3",
+        "X7"
+    };
+
+    int a=0;
+
+    for (int i=0; i<5; i++)
+    {
+        cout <<endl;
+        cout <<"BRAND:" <<brand[i] << " " << endl;
+        cout <<"MODELI: ";
+        for (int j=0; j<4; j++)
+        {
+            cout<< model[a++] <<", ";
+            if (j==4) printf("\n");
+        }
 
 
-for (int i=0;i<5;i++)
-{
-    cout <<"Auto:" <<brand[i] <<" "<< model[i] <<endl;
-}
+    }
 
 
 
