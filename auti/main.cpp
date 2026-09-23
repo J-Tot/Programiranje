67a0e1ef895522b4a9c155f07866476e5a6fa155
JTot <mario.jakov.tot@gmail.com>
2026-09-23 19:20:31 +0200

    Program ver_1

---
+++ auti/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+string brand[5]={
+"Ferrari",
+"Mercedes",
+"Renault",
+"Porsche",
+"BMW"
+};
+string model[5]={
+"SF24",
+"G Klasa",
+"Clio",
+"911",
+"X5",
+};
+
+
+for (int i=0;i<5;i++)
+{
+    cout <<"Auto:" <<brand[i] <<" "<< model[i] <<endl;
+}
+
+
+
+
+
+
+
+
+
+    return 0;
+}
